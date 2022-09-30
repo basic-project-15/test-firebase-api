037412cfc9067b14f2c89241410eb15f4fba119e
XxLuisFer15xX <luisfer.sm15@gmail.com>
2022-09-30 09:44:07 -0600

    feat: Hablitando cors.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
 
 const {
   getTasks,
@@ -18,47 +19,69 @@ const { postLogin } = require('./controllers/auth');
 
 // Tasks
 exports.getTasks = functions.https.onRequest(async (req, res) => {
-  getTasks(req, res);
+  cors(req, res, () => {
+    getTasks(req, res);
+  });
 });
 
 exports.getTask = functions.https.onRequest(async (req, res) => {
-  getTask(req, res);
+  cors(req, res, () => {
+    getTask(req, res);
+  });
 });
 
 exports.postTask = functions.https.onRequest(async (req, res) => {
-  postTask(req, res);
+  cors(req, res, () => {
+    postTask(req, res);
+  });
 });
 
 exports.patchTask = functions.https.onRequest(async (req, res) => {
-  patchTask(req, res);
+  cors(req, res, () => {
+    patchTask(req, res);
+  });
 });
 
 exports.deleteTask = functions.https.onRequest(async (req, res) => {
-  deleteTask(req, res);
+  cors(req, res, () => {
+    deleteTask(req, res);
+  });
 });
 
 // Users
 exports.getUsers = functions.https.onRequest(async (req, res) => {
-  getUsers(req, res);
+  cors(req, res, () => {
+    getUsers(req, res);
+  });
 });
 
 exports.getUser = functions.https.onRequest(async (req, res) => {
-  getUser(req, res);
+  cors(req, res, () => {
+    getUser(req, res);
+  });
 });
 
 exports.postUser = functions.https.onRequest(async (req, res) => {
-  postUser(req, res);
+  cors(req, res, () => {
+    postUser(req, res);
+  });
 });
 
 exports.patchUser = functions.https.onRequest(async (req, res) => {
-  patchUser(req, res);
+  cors(req, res, () => {
+    patchUser(req, res);
+  });
 });
 
 exports.deleteUser = functions.https.onRequest(async (req, res) => {
-  deleteUser(req, res);
+  cors(req, res, () => {
+    deleteUser(req, res);
+  });
 });
 
 // Auth
 exports.postLogin = functions.https.onRequest(async (req, res) => {
-  postLogin(req, res);
+  cors(req, res, () => {
+    postLogin(req, res);
+  });
 });
